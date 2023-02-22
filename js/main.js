69fc1f746fde67413f52f1939f7275d6f76c4817
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-02-22 08:25:07 -0600

    clases edited

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// # Clase animal // Padre
-// 	● Nombre común
-//   ● Nombre científico
-//   ● Tipo de sangre: Fría / Caliente
-//   ● Dieta: Carnívoro / Omnívoro / Herbívoro
-//   ● Reproducción: Ovíparo / Vivíparo
-//   ● Hábitat: Tierra / Agua / Ambos
-
-//   ● Patas (cantidad)
-//   ● Escamas (color)
-
-//   ● Aletas (cantidad)
-
-//   ● Vertebrados
-//   ● Invertebrados (3)
-
 class Animal {
   constructor(nombreComun, nombreCientifico, tipoSangre, dieta, reproduccion, habitat) {
     this.nombreComun = nombreComun;
@@ -22,6 +6,7 @@ class Animal {
     this.dieta = dieta;
     this.reproduccion = reproduccion;
     this.habitat = habitat;
+    this.escamas = escamas;
   }
 }
 
@@ -29,33 +14,31 @@ class Mamifero extends Animal{
   constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,pelo){
     super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat);
     this.pelo = pelo;
- }
+  }
 }
 
 class Peces extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,aletas){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
+  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,aletas, escamas){
+    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, escamas)
     this.aletas = aletas;
- }
+  }
 }
 
 class Reptiles extends Animal{
   constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,escamas){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
-    this.escamas = escamas;
- }
+    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, escamas)
+  }
 }
 
 class Anfibio extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,escamas){
+  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat){
     super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
-    this.escamas = escamas;
- }
+  }
 }
 
 class Aves extends Animal{
   constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,plumas){
     super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
     this.plumas = plumas;
- }
+  }
 }
