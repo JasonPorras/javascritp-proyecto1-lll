03356140afa72316820ac9c5a86433e2f7974689
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-02-25 11:20:43 -0600

    galleria de imagenes agregada

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 class Animal {
-  constructor(nombreComun, nombreCientifico, tipoSangre, dieta, reproduccion, habitat) {
+  constructor(nombreComun, nombreCientifico, tipoSangre, dieta, reproduccion, habitat, imagen) {
     this.nombreComun = nombreComun;
     this.nombreCientifico = nombreCientifico;
     this.tipoSangre = tipoSangre;
     this.dieta = dieta;
     this.reproduccion = reproduccion;
     this.habitat = habitat;
-    this.escamas = escamas;
+    this.imagen = imagen;
+  }
+
+  paint(d) {
+    document.getElementById('gallery').innerHTML = d.map(item => {
+      return `
+        <li class="flex">
+          <img src="${item.imagen}" alt="${item.nombreComun}">
+          <h3>${item.nombreComun}</h3>
+        </li>
+      `
+    }).join('')
   }
 }
+
 
 class Mamifero extends Animal{
   constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,pelo){
@@ -19,20 +31,24 @@ class Mamifero extends Animal{
 
 class Peces extends Animal{
   constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,aletas, escamas){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, escamas)
+    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
     this.aletas = aletas;
+    this.escamas = escamas;
   }
 }
 
 class Reptiles extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,escamas){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, escamas)
+  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,escamas, imagen){
+    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, imagen)
+    this.escamas = escamas;
   }
+
+  
 }
 
 class Anfibio extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
+  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, imagen){
+    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, imagen)
   }
 }
 
@@ -42,3 +58,15 @@ class Aves extends Animal{
     this.plumas = plumas;
   }
 }
+
+const gallery = [
+  new Reptiles('Caimán de Cuvier', 'Paleosuchus palpebrosus', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'Verde Musgo', 'img/caiman-cuvier.jpeg'),
+  new Reptiles('Anaconda común', 'Eunectes murinus', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'Amarillas y manchas oscuras de color marrón y negro', 'img/anaconda.jpeg'),
+  new Reptiles('Iguana rinoceronte', 'Cyclura cornuta', 'Fría', 'Herbívoro', 'Ovíparo', 'Tierra', 'Marrón-grisáceo o verde oliva', 'img/iguana-rinoceronte.jpeg'),
+  new Anfibio('Rana', 'Anura', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'img/rana.jpeg'),
+  new Anfibio('Sapo', 'Bufonidae', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'img/sapo.jpeg'),
+  new Anfibio('Salamandra', 'Urodela', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua', 'img/salamandra.jpeg'),
+]
+const animals = new Animal()
+animals.paint(gallery)
+
