0431d95af402da3c1702185e6b54362ad65d36a9
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-02-27 13:03:17 -0600

    feature: filter animals finished

--- js/main.js
+++ js/main.js
@@ -1,72 +1,269 @@
 class Animal {
-  constructor(nombreComun, nombreCientifico, tipoSangre, dieta, reproduccion, habitat, imagen) {
-    this.nombreComun = nombreComun;
-    this.nombreCientifico = nombreCientifico;
-    this.tipoSangre = tipoSangre;
-    this.dieta = dieta;
-    this.reproduccion = reproduccion;
-    this.habitat = habitat;
-    this.imagen = imagen;
+  constructor(
+    nombreComun,
+    nombreCientifico,
+    tipoSangre,
+    dieta,
+    reproduccion,
+    habitat,
+    imagen,
+    claseAnimal,
+  ) {
+    this.nombreComun = nombreComun
+    this.nombreCientifico = nombreCientifico
+    this.tipoSangre = tipoSangre
+    this.dieta = dieta
+    this.reproduccion = reproduccion
+    this.habitat = habitat
+    this.imagen = imagen
+    this.claseAnimal = claseAnimal
   }
 
-  paint(d) {
-    document.getElementById('gallery').innerHTML = d.map(item => {
+  paintJustImages(arrGallery) {
+    document.getElementById('gallery').innerHTML = arrGallery.map((item) => {
       return `
-        <li class="flex">
-          <img src="${item.imagen}" alt="${item.nombreComun}">
-          <h3>${item.nombreComun}</h3>
-        </li>
-      `
+      <li class="flex">
+        <img src="${item.imagen}" alt="${item.nombreComun}">
+        <h3>${item.nombreComun}</h3>
+      </li>
+    `
     }).join('')
   }
 }
 
-
-class Mamifero extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,pelo){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat);
-    this.pelo = pelo;
+class Mamifero extends Animal {
+  constructor(
+    nombreComun,
+    nombreCientifico,
+    tipoSangre,
+    dieta,
+    reproduccion,
+    habitat,
+    pelo,
+    imagen,
+    claseAnimal,
+  ) {
+    super(
+      nombreComun,
+      nombreCientifico,
+      tipoSangre,
+      dieta,
+      reproduccion,
+      habitat,
+      imagen,
+      claseAnimal,
+    )
+    this.pelo = pelo
   }
 }
 
-class Peces extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,aletas, escamas){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
-    this.aletas = aletas;
-    this.escamas = escamas;
+class Peces extends Animal {
+  constructor(
+    nombreComun,
+    nombreCientifico,
+    tipoSangre,
+    dieta,
+    reproduccion,
+    habitat,
+    aletas,
+    escamas,
+    imagen,
+    claseAnimal,
+  ) {
+    super(
+      nombreComun,
+      nombreCientifico,
+      tipoSangre,
+      dieta,
+      reproduccion,
+      habitat,
+      imagen,
+      claseAnimal,
+    )
+    this.aletas = aletas
+    this.escamas = escamas
   }
 }
 
-class Reptiles extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,escamas, imagen){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, imagen)
-    this.escamas = escamas;
+class Reptiles extends Animal {
+  constructor(
+    nombreComun,
+    nombreCientifico,
+    tipoSangre,
+    dieta,
+    reproduccion,
+    habitat,
+    escamas,
+    imagen,
+    claseAnimal,
+  ) {
+    super(
+      nombreComun,
+      nombreCientifico,
+      tipoSangre,
+      dieta,
+      reproduccion,
+      habitat,
+      imagen,
+      claseAnimal,
+    )
+    this.escamas = escamas
   }
-
-  
 }
 
-class Anfibio extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, imagen){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat, imagen)
+class Anfibio extends Animal {
+  constructor(
+    nombreComun,
+    nombreCientifico,
+    tipoSangre,
+    dieta,
+    reproduccion,
+    habitat,
+    imagen,
+    claseAnimal,
+  ) {
+    super(
+      nombreComun,
+      nombreCientifico,
+      tipoSangre,
+      dieta,
+      reproduccion,
+      habitat,
+      imagen,
+      claseAnimal,
+    )
   }
 }
 
-class Aves extends Animal{
-  constructor(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat,plumas){
-    super(nombreComun,nombreCientifico,tipoSangre,dieta,reproduccion,habitat)
-    this.plumas = plumas;
+class Aves extends Animal {
+  constructor(
+    nombreComun,
+    nombreCientifico,
+    tipoSangre,
+    dieta,
+    reproduccion,
+    habitat,
+    plumas,
+    imagen,
+    claseAnimal,
+  ) {
+    super(
+      nombreComun,
+      nombreCientifico,
+      tipoSangre,
+      dieta,
+      reproduccion,
+      habitat,
+      imagen,
+      claseAnimal,
+    )
+    this.plumas = plumas
   }
 }
 
 const gallery = [
-  new Reptiles('Caimán de Cuvier', 'Paleosuchus palpebrosus', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'Verde Musgo', 'img/caiman-cuvier.jpeg'),
-  new Reptiles('Anaconda común', 'Eunectes murinus', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'Amarillas y manchas oscuras de color marrón y negro', 'img/anaconda.jpeg'),
-  new Reptiles('Iguana rinoceronte', 'Cyclura cornuta', 'Fría', 'Herbívoro', 'Ovíparo', 'Tierra', 'Marrón-grisáceo o verde oliva', 'img/iguana-rinoceronte.jpeg'),
-  new Anfibio('Rana', 'Anura', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'img/rana.jpeg'),
-  new Anfibio('Sapo', 'Bufonidae', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua y Tierra', 'img/sapo.jpeg'),
-  new Anfibio('Salamandra', 'Urodela', 'Fría', 'Carnívoro', 'Ovíparo', 'Agua', 'img/salamandra.jpeg'),
+  new Reptiles(
+    'Caimán de Cuvier',
+    'Paleosuchus palpebrosus',
+    'Fría',
+    'Carnívoro',
+    'Ovíparo',
+    'Agua y Tierra',
+    'Verde Musgo',
+    'img/caiman-cuvier.jpeg',
+    'reptil',
+  ),
+  new Reptiles(
+    'Anaconda común',
+    'Eunectes murinus',
+    'Fría',
+    'Carnívoro',
+    'Ovíparo',
+    'Agua y Tierra',
+    'Amarillas y manchas oscuras de color marrón y negro',
+    'img/anaconda.jpeg',
+    'reptil',
+  ),
+  new Reptiles(
+    'Iguana rinoceronte',
+    'Cyclura cornuta',
+    'Fría',
+    'Herbívoro',
+    'Ovíparo',
+    'Tierra',
+    'Marrón-grisáceo o verde oliva',
+    'img/iguana-rinoceronte.jpeg',
+    'reptil',
+  ),
+  new Anfibio(
+    'Rana',
+    'Anura',
+    'Fría',
+    'Carnívoro',
+    'Ovíparo',
+    'Agua y Tierra',
+    'img/rana.jpeg',
+    'anfibio',
+  ),
+  new Anfibio(
+    'Sapo',
+    'Bufonidae',
+    'Fría',
+    'Carnívoro',
+    'Ovíparo',
+    'Agua y Tierra',
+    'img/sapo.jpeg',
+    'anfibio',
+  ),
+  new Anfibio(
+    'Salamandra',
+    'Urodela',
+    'Fría',
+    'Carnívoro',
+    'Ovíparo',
+    'Agua',
+    'img/salamandra.jpeg',
+    'anfibio',
+  ),
 ]
 const animals = new Animal()
-animals.paint(gallery)
+animals.paintJustImages(gallery)
 
+
+// esta funcion filtra los animales
+function filterAnimals(value) {
+  const result = gallery.filter((item) => {
+    if (value === 'todos') {
+      return gallery
+    }
+    
+    if (value === item.claseAnimal.toLowerCase()) {
+      return item
+    } else if (value === item.tipoSangre.toLowerCase()) {
+      return item
+    } else if (value === item.dieta.toLowerCase()) {
+      return item
+    }
+     else if (value === item.reproduccion.toLowerCase()) {
+      return item
+    } else if (value === item.habitat.toLowerCase()) {
+      return item
+    }
+  })
+
+  animals.paintJustImages(result)
+}
+
+// habilitar, deshabilitar options y enviar value
+const radios = document.querySelectorAll('input[type="radio"][name="animal"]')
+radios.forEach((radio) => {
+  radio.addEventListener('change', (e) => {
+    filterAnimals(e.target.value)
+
+    radios.forEach((otherRadio) => {
+      if (otherRadio !== radio) {
+        otherRadio.checked = false
+      }
+    })
+  })
+})
